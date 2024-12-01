c8c87c6202bb28e759e2847e2149b410496e84df
Frank Latos <flatos@duosys.com>
2024-12-01 15:52:31 -0500

    A5p2 try 1

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -11,7 +11,10 @@
 # AESD_ASSIGNMENTS_VERSION = 5a3d163		#'#COMMIT VERSION NUMBER'
 # AESD_ASSIGNMENTS_VERSION = c2d8cf4		#'#COMMIT VERSION NUMBER'
 # AESD_ASSIGNMENTS_VERSION = 63cc346		#'#COMMIT VERSION NUMBER'
-AESD_ASSIGNMENTS_VERSION = 01f3ce2		#'#COMMIT VERSION NUMBER'
+# AESD_ASSIGNMENTS_VERSION = 01f3ce2		#'#COMMIT VERSION NUMBER'
+# AESD_ASSIGNMENTS_VERSION = 63bb22f		#'#COMMIT VERSION NUMBER'
+AESD_ASSIGNMENTS_VERSION = b43c395		#'#COMMIT VERSION NUMBER'
+
 
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
@@ -25,6 +28,7 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -36,6 +40,11 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+
+
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
 endef
 
 $(eval $(generic-package))
